cd3fe3c22d67eab01a9d66310bce61275f90a50c
Victor Vasconcelos <contato@victorvasconcelos.com>
2024-08-28 16:54:00 -0300

    first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,6 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 require('dotenv').config();
 
 /** @type {import('@docusaurus/types').Config} */
@@ -104,11 +102,7 @@ const config = {
             ],
           },
         ],
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
+      }
     }),
 };
 
